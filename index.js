aa91b670853b0be766fa8ddd054c4e4ebdb9d5be
Rita Musello-Kelliher <ritamkell@gmail.com>
2019-01-16 02:21:46 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,36 @@
+function shout(string) {
+return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log('HELLO')
+  return string.toUpperCase()
+}
+function logWhisper(string) {
+  console.log('hello')
+  return string.toLowerCase
+}
+
+var uppercase = "HELLO!"
+uppercase.toUpperCase() === uppercase
+var lowercase = "hello!"
+lowercase.toLowerCase() === lowercase
+var mixedCase = "Hi there."
+mixedCase.toLowerCase() === mixedCase
+mixedCase.toUpperCase() === mixedCase
+
+function sayHiToGrandma(arg) {
+  if (arg === lowercase) {
+  console.log("I can't hear you!") 
+  return "I can't hear you!"
+  } else if (arg === uppercase) {
+    return "YES INDEED!"
+    console.log("YES INDEED")
+  } else if (arg === mixedCase)
+  return "I love you, too!"
+  console.log("I love you, too.")
+  } 
+  
+
